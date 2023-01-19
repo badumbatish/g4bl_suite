1f4bb345355f936abfd6492d20e43a768deaada4
Jasmine T <tanghocle456@gmail.com>
2023-01-19 11:13:49 -0600

    Fix figure size, save figure via pdf, work on 1 detector now

--- plot.py
+++ plot.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from os.path import exists
 
-def add_file():
-    data = [None] * 8
+# add feature count
+feature_count = 4
 
-    for i in range(0, 8):
+# add feature name: x position, y postion and stuff
+feature_list = ["x", "y", "Xp", "Yp"]
+
+def add_file():
+    # returns a 2D numpy array that formats just like the output txt file from G4Beamline
+    num_detector = 1
+    data = [None] * num_detector
+    for i in range(num_detector):
         ascii_output_file =  "detector" + str(i+1) + ".txt"
         if(exists(ascii_output_file)):
             data[i] = np.loadtxt(ascii_output_file)
@@ -13,18 +20,7 @@ def add_file():
 
 data = add_file()
 
-def fig_plot(fig, axes, data, plot_type = "position"):
-    fig.suptitle("Gaussian beam " + plot_type + " graph", fontsize="16")
-    fig.supxlabel("x " + plot_type)
-    fig.supylabel("y " + plot_type)
-    plot(axes[0], data[0], plot_type)
-    axes[0].set_title(f"Detector 1 (4mm away from beam source)")
-
-    plot(axes[1], data[7], plot_type)
-    axes[1].set_title(f"Detector 8 (5915mm away from beam source)")
-
-
-def plot(axes,data,plot_type = "position"):
+def scatter_plot(axes,data,plot_type = "position"):
     # The second input of data[] is the column
     # 0: x position
     # 1: y position
@@ -32,18 +28,44 @@ def plot(axes,data,plot_type = "position"):
     # 3: Yp position
     if plot_type == "position" :
         x = data[:,0]
-        y= data[:,1]
-        axes.scatter(x,y)
+        y = data[:,1]
     elif plot_type == "momentum" :
         x = data[:,2]
         y = data[:,3]
-        axes.scatter(x,y)
+    axes.scatter(x,y, rasterized=False)
 
-fig1, axes1 = plt.subplots(2, sharex=True, sharey=True)
-fig_plot(fig1, axes1, data, "position")
-plt.savefig('pics/gaussian_position_report.png', dpi=1200)
+def hist_plot(axes, data):
+    for i in range(4):
+        axes[i].hist(data[:,i])
+        axes[i].set_xlabel(feature_list[i])
+        axes[i].set_xticks
 
-fig2, axes2 = plt.subplots(2, sharex=True, sharey=True)
-fig_plot(fig2, axes2, data, "momentum")
-plt.savefig('pics/gaussian_momentum_report.png', dpi=1200)
-plt.show()
+
+def save_figure(fig, file_name, dpi=400):
+    fig.set_size_inches((8.5, 11), forward=False)
+    fig.savefig(file_name, dpi=dpi)
+
+beam_type = "normal"
+gaussian_position_fig, gaussian_position_axes = plt.subplots(1, sharex=True, sharey=True, layout="constrained")
+gaussian_position_fig.suptitle(f"{beam_type.capitalize()} beam position")
+gaussian_position_fig.supxlabel("x position")
+gaussian_position_fig.supylabel("y position")
+scatter_plot(gaussian_position_axes, data[0])
+# gaussian_position_axes.set_title("Detector 1 (4mm away from beam source")
+save_figure(gaussian_position_fig, f"pics/{beam_type}_position.pdf")
+
+
+gaussian_momentum_fig, gaussian_momentum_axes = plt.subplots(1, sharex=True, sharey=True, layout="constrained")
+gaussian_momentum_fig.suptitle(f"{beam_type.capitalize()} beam momentum")
+gaussian_momentum_fig.supxlabel("x momentum")
+gaussian_momentum_fig.supylabel("y momentum")
+scatter_plot(gaussian_momentum_axes, data[0], plot_type="momentum")
+# gaussian_momentum_axes.set_title("Detector 1 (4mm away from beam source")
+save_figure(gaussian_momentum_fig,  f"pics/{beam_type}_momentum.pdf")
+
+
+gaussian_hist_fig, gaussian_hist_axes = plt.subplots(feature_count, sharey=True, layout="constrained")
+gaussian_hist_fig.supylabel("count")
+hist_plot(gaussian_hist_axes, data[0])
+gaussian_hist_fig.set_size_inches((8.5, 11), forward=False)
+gaussian_hist_fig.savefig(f"pics/{beam_type}_hist.pdf", dpi=500)

89d7d32b0e7f0403104bc1f4a24ad4533bbb4a7f
Jasmine T <tanghocle456@gmail.com>
2023-02-28 16:16:29 -0800

    Add test case for single list

--- tests/test_generate_args.py
+++ tests/test_generate_args.py
@@ -38,7 +38,7 @@ def test_str_list_with_int_list():
         [g4bl_cmd,file_name,'_meanMomentum=200','angle=3'],
         [g4bl_cmd,file_name,'_meanMomentum=300','angle=1'],
         [g4bl_cmd,file_name,'_meanMomentum=300','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=3'],
+        [g4bl_cmd,file_name,'_meanMomentum=300','angle=3']
     ]
     assert generated_args == test_args
 def test_mixed_list_with_intfloat_list():
@@ -84,5 +84,16 @@ def test_tuple_zipl():
     test_args = [
         [g4bl_cmd,file_name,'_meanMomentum=100','a=4','b=6'],
         [g4bl_cmd,file_name,'_meanMomentum=100','a=5','b=7']
+    ]
+    assert generated_args == test_args
+
+def test_singular_tuple():
+    param_dict = {
+        "sigma": [10],
+        ("Magnet","QUADgradient13", "QUADgradient2", "SOLcurrent") : (["Quadx1.5SolOFF", 0     ,0    ,0],)
+    }
+    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    test_args = [
+        [g4bl_cmd, file_name, 'sigma=10', 'Magnet=Quadx1.5SolOFF',"QUADgradient13=0","QUADgradient2=0","SOLcurrent=0"]
     ]
     assert generated_args == test_args

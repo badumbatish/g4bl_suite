56688a1b9e12de09ce9a15878a00d867e3779239
Jasmine T <tanghocle456@gmail.com>
2023-07-20 00:55:24 -0500

    Add test cases and test data for remembrance

--- tests/unit/test_remembrance.py
+++ tests/unit/test_remembrance.py
@@ -118,6 +118,27 @@ def test_construct_list_files():
     assert g4blplot.construct_list_files(filtered_arg_list=filtered_arg_list,
                                          postfix_string_list = postfix_string_list) == test_args
 
+def test_all_file_exists():
+
+    # All file exists
+    all_lst = [
+                '_meanMomentum100|angle1|detector1.txt',
+                '_meanMomentum100|angle1|detector2.txt',
+                ]
+    assert g4blplot.all_file_exists(all_lst, data_directory= None, test = True) == True
+
+
+    # Half of list of file exists, should return False
+    half_lst = [
+                
+                '_meanMomentum200|angle1|detector1.txt',
+                '_meanMomentum200|angle1|detector2.txt',
+            ]
+
+    assert g4blplot.all_file_exists(half_lst, data_directory= None, test = True) == False
+
+
+
 def test_get_index_of_needed_tasks():
     """
         Test the function to see if it is truly getting the index of the needed tasks, not the already computed tasks
@@ -127,4 +148,33 @@ def test_get_index_of_needed_tasks():
             - the function is returning a list, then it should return all a list of 4 elements, where 1st and 2nd element is true, 0th and 4th is false
             - the function is returning a set, then it should return a set of all needed integers only
     """
-    pass
+    postfix_string_list = [ "detector1",
+                            "detector2",
+                          ]
+    filtered_arg_list = [
+       [ '_meanMomentum=100','angle=1'],
+       [ '_meanMomentum=100','angle=2'],
+       [ '_meanMomentum=100','angle=3'],
+       [ '_meanMomentum=200','angle=1'],
+       [ '_meanMomentum=200','angle=2'],
+       [ '_meanMomentum=200','angle=3'],
+       [ '_meanMomentum=300','angle=1'],
+       [ '_meanMomentum=300','angle=2'],
+       [ '_meanMomentum=300','angle=3'],
+    ]
+
+    data_list_of_list =  g4blplot.construct_list_files(filtered_arg_list=filtered_arg_list,
+                                         postfix_string_list = postfix_string_list)
+
+    result_list = []
+
+    # First 3 element is True, middle 5 element is False, last element is True
+    for _ in range(3):
+        result_list.append(True)
+    for _ in range(5):
+        result_list.append(False)
+    for _ in range(1):
+        result_list.append(True)
+    
+    assert True
+    assert g4blplot.get_index_of_needed_tasks(data_list = data_list_of_list, data_directory = None, test = True) == result_list

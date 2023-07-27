c7715abc01478d2aaa384a7667bebac03fee7187
Jasmine T <tanghocle456@gmail.com>
2023-07-26 22:43:58 -0500

    Fix linting mistakes

--- src/g4blplot/g4blplot.py
+++ src/g4blplot/g4blplot.py
@@ -1,22 +1,36 @@
-import matplotlib.pyplot as plt
-import numpy as np
-import mpl_scatter_density
-import multiprocessing as mp
 import itertools
+import multiprocessing as mp
 import subprocess
-import tqdm
 from os.path import exists
+
+import matplotlib.pyplot as plt
+import mpl_scatter_density
+import numpy as np
 import pandas as pd
+import tqdm
 
-feature_list = ["x", "y", "z", "Px", "Py", "Pz", "t", "PDGid", "EventID", "TrackID", "ParentID", "Weight"]
-feature_dict = {key : value for (value,key) in enumerate(feature_list)} 
+feature_list = [
+    "x",
+    "y",
+    "z",
+    "Px",
+    "Py",
+    "Pz",
+    "t",
+    "PDGid",
+    "EventID",
+    "TrackID",
+    "ParentID",
+    "Weight",
+]
+feature_dict = {key: value for (value, key) in enumerate(feature_list)}
 
-particle_dict = {"pi-": -211,
-                "mu-": 13,
-                "mu+": -13}
+particle_dict = {"pi-": -211, "mu-": 13, "mu+": -13}
 
-def add_text_file(file_name : str) :
-    """Returns a 2D numpy array that formats just like the output txt file from G4Beamline and raise exception if file does not exists
+
+def add_text_file(file_name: str):
+    """Returns a 2D numpy array that formats just like the output txt file
+        from G4Beamline and raise exception if file does not exists
 
     Parameters
     ----------
@@ -28,24 +42,25 @@ def add_text_file(file_name : str) :
         a 2D numpy array
     """
 
-    if(exists(file_name)):
+    if exists(file_name):
         data = np.loadtxt(file_name)
     else:
         raise Exception(f"The file {file_name} does not exist")
-    
+
     if len(data.shape) == 1:
-        data = data[np.newaxis,:]
+        data = data[np.newaxis, :]
     return data
 
-def scatter_plot(axes, x_axis, y_axis, heat_map:bool = False) :
-    """ Scatter plot an axes based on 2 1D numpy array
+
+def scatter_plot(axes, x_axis, y_axis, heat_map: bool = False):
+    """Scatter plot an axes based on 2 1D numpy array
     Parameters
     ----------
-    axes : 
-        an object subplot from matplotlib that is a  
-    x_axis : 
+    axes :
+        an object subplot from matplotlib that is a
+    x_axis :
         1D array that denotes what to plot on the x axis
-    y_axis : 
+    y_axis :
         1D array that denotes what to plot on the y axis
 
     Returns
@@ -53,11 +68,12 @@ def scatter_plot(axes, x_axis, y_axis, heat_map:bool = False) :
         Function returns nothing, only plots a graph to the axes
     """
 
-    if(heat_map == False):
-        axes.scatter(x_axis,y_axis, rasterized=False)
+    if heat_map == False:
+        axes.scatter(x_axis, y_axis, rasterized=False)
     else:
-        density = axes.scatter_density(x_axis,y_axis)
-        plt.colorbar(density, ax = axes, label='Number of points per pixel')
+        density = axes.scatter_density(x_axis, y_axis)
+        plt.colorbar(density, ax=axes, label="Number of points per pixel")
+
 
 def hist_plot(axes, data, xlabel: str = ""):
     """
@@ -78,7 +94,8 @@ def hist_plot(axes, data, xlabel: str = ""):
         axes.set_xlabel(xlabel)
         axes.set_ylabel(f"Count of {xlabel}")
     stats_str = f"Count: {data.size}\nMean: {data.mean():.3f}\nStd: {data.std():.3f}\nMin: {data.min()}\n25%: {np.percentile(data,25)}\n50%: {np.percentile(data,50)}\n75%: {np.percentile(data,75)}\nMax: {data.max()}"
-    axes.text(1.01,0.2, stats_str ,transform=plt.gca().transAxes)
+    axes.text(1.01, 0.2, stats_str, transform=plt.gca().transAxes)
+
 
 def set_fig_misc(fig, beam_type, plot_type):
     """Sets miscellaneous features of a figure
@@ -97,11 +114,12 @@ def set_fig_misc(fig, beam_type, plot_type):
     fig.supxlabel(f"x {plot_type}")
     fig.supylabel(f"y {plot_type}")
 
+
 def save_figure(fig, file_name, dpi=300):
-    """Save the figure into a file, usually pdf
-    """
+    """Save the figure into a file, usually pdf"""
     fig.set_size_inches((8.5, 11), forward=False)
     fig.savefig(file_name, dpi=dpi)
+
 
 def extract_particle_data(data, particle_name=None, particle_id=None):
     """Extracts a numpy array of only a certain particle out of a raw data
@@ -115,29 +133,32 @@ def extract_particle_data(data, particle_name=None, particle_id=None):
     mask = None
     # make a 1D array mask that returns true if the PID is satisfy
     # [:,7] represents the column of PIDs
-    if(particle_id is not None):
-        mask = data[:,feature_dict["PDGid"]] == particle_id
-    elif(particle_name is not None):
-        mask = (data[:,feature_dict["PDGid"]] == particle_dict[particle_name])
+    if particle_id is not None:
+        mask = data[:, feature_dict["PDGid"]] == particle_id
+    elif particle_name is not None:
+        mask = data[:, feature_dict["PDGid"]] == particle_dict[particle_name]
     else:
         raise Exception(f"Particle id or particle name is not present")
 
     # pass the mask to the raw data to select the PID-satisfying rows, then : to select all the columns of that row
-    particle_data = data[mask,:]
+    particle_data = data[mask, :]
 
     return particle_data
 
+
 def get_feature(data, feature_name):
-    return data[:,feature_dict[feature_name]]
-    
+    return data[:, feature_dict[feature_name]]
+
+
 def get_xangle(data):
     """
-        This function returns a 1D array consisting of xp = Px/Pz in milliradian
+    This function returns a 1D array consisting of xp = Px/Pz in milliradian
     """
-    Px = data[:,feature_dict["Px"]]
-    Pz = data[:,feature_dict["Pz"]]
+    Px = data[:, feature_dict["Px"]]
+    Pz = data[:, feature_dict["Pz"]]
 
-    return (Px/Pz)*1000
+    return (Px / Pz) * 1000
+
 
 def tuple_zipl(args):
     """Return a tuple of list from the argument being a list of tuples"""
@@ -148,71 +169,84 @@ def tuple_zipl(args):
     a = tuple(tp)
     return a
 
+
 def get_yangle(data):
     """
-        This function returns a 1D array consisting of yp = Py/Pz in milliradian
+    This function returns a 1D array consisting of yp = Py/Pz in milliradian
     """
-    Py = data[:,feature_dict["Py"]]
-    Pz = data[:,feature_dict["Pz"]]
+    Py = data[:, feature_dict["Py"]]
+    Pz = data[:, feature_dict["Pz"]]
 
-    return (Py/Pz)*1000
+    return (Py / Pz) * 1000
+
 
 def get_particle_count(data, particle_name=None, particle_id=None):
-    if(particle_id is not None):
-        res = np.count_nonzero(data[:,feature_dict["PDGid"]] == particle_id)
-    elif(particle_name is not None):
-        res = np.count_nonzero(data[:,feature_dict["PDGid"]] == particle_dict[particle_name])
+    if particle_id is not None:
+        res = np.count_nonzero(data[:, feature_dict["PDGid"]] == particle_id)
+    elif particle_name is not None:
+        res = np.count_nonzero(
+            data[:, feature_dict["PDGid"]] == particle_dict[particle_name]
+        )
     else:
         raise Exception(f"Particle id or particle name is not present")
     return res
+
+
 def run_command(args):
     """
-        Helper function for automate()
+    Helper function for automate()
     """
-    #print(f"Running {args}")
-    result = subprocess.run(args,stdout=subprocess.DEVNULL)
+    # print(f"Running {args}")
+    result = subprocess.run(args, stdout=subprocess.DEVNULL)
+
 
 def isG4BL(cmd: str):
     return cmd.endswith("g4bl")
+
+
 def isG4BLMPI(cmd: str):
     return cmd.endswith("g4blmpi")
 
+
 def generate_args(cmd: str, param_dict: dict, file_name: str, mpi_count=None):
     """
-        Generates a list of arguments that is the first parameter for subprocess.run
+    Generates a list of arguments that is the first parameter for subprocess.run
     """
     for key, value in param_dict.items():
         key = tuple(value)
 
     keys = []
     for element in param_dict.keys():
-        if not isinstance(element,tuple):
+        if not isinstance(element, tuple):
             keys.append(element)
         else:
             keys.extend(element)
-    
+
     values = []
     for element in param_dict.values():
-        if not isinstance(element,tuple):
+        if not isinstance(element, tuple):
             values.append(element)
         else:
             values.append(list(v for v in element))
-    
+
     combination = list(itertools.product(*param_dict.values()))
 
     # combination =  list(itertools.product(*values))
     def flatten(A):
         rt = []
         for i in A:
-            if isinstance(i,list): rt.extend(flatten(i))
-            else: rt.append(i)
+            if isinstance(i, list):
+                rt.extend(flatten(i))
+            else:
+                rt.append(i)
         return rt
+
     args = []
     for each_combination in combination:
         lst = []
         lst.append(cmd)
-        if(isG4BLMPI(cmd)):
-            lst.append(str(mpi_count)) 
+        if isG4BLMPI(cmd):
+            lst.append(str(mpi_count))
         lst.append(file_name)
         each_combination = flatten(each_combination)
         for i, value in enumerate(each_combination):
@@ -222,17 +256,28 @@ def generate_args(cmd: str, param_dict: dict, file_name: str, mpi_count=None):
 
     return args
 
-def automate(cmd: str, param_dict: dict, file_name : str,total_process_count = 1, mpi_count = None):
-    """
-    """
-    args  = generate_args(cmd,param_dict, file_name,str(mpi_count))
+
+def automate(
+    cmd: str, param_dict: dict, file_name: str, total_process_count=1, mpi_count=None
+):
+    """ """
+    args = generate_args(cmd, param_dict, file_name, str(mpi_count))
     process_count = 0
-    if(mpi_count is None):
+    if mpi_count is None:
         process_count = int(total_process_count)
     else:
         process_count = int(total_process_count / int(mpi_count))
-        
-    print(f"Creating pool with total process count = {total_process_count}, pool process count = {process_count}, G4BLMPI process count = {mpi_count}")
+
+    print(
+        f"Creating pool with total process count = {total_process_count}, pool process count = {process_count}, G4BLMPI process count = {mpi_count}"
+    )
     with mp.Pool(process_count) as p:
         # color is pastel pink hehe
-        list(tqdm.tqdm(p.imap_unordered(run_command, args), total=len(args),colour="#F8C8DC", desc="Batch progress bar"))
+        list(
+            tqdm.tqdm(
+                p.imap_unordered(run_command, args),
+                total=len(args),
+                colour="#F8C8DC",
+                desc="Batch progress bar",
+            )
+        )
--- tests/test_generate_args.py
+++ tests/test_generate_args.py
@@ -1,100 +1,115 @@
-from g4blplot.g4blplot import generate_args
-from g4blplot.g4blplot import tuple_zipl
+from g4blplot.g4blplot import generate_args, tuple_zipl
 
 g4bl_cmd = "g4bl"
 g4blmpi_cmd = "g4blmpi"
 file_name = "file_name"
 
+
 def test_int_list_with_int_list():
-    param_dict = {
-        "_meanMomentum": [100,200,300],
-        "angle": [1,2,3]
-    }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    param_dict = {"_meanMomentum": [100, 200, 300], "angle": [1, 2, 3]}
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=3'],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=3"],
     ]
     assert generated_args == test_args
 
+
 def test_str_list_with_int_list():
-    param_dict = {
-        "_meanMomentum": ["100","200","300"],
-        "angle": [1,2,3]
-    }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    param_dict = {"_meanMomentum": ["100", "200", "300"], "angle": [1, 2, 3]}
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=1'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=3']
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=1"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=3"],
     ]
     assert generated_args == test_args
+
+
 def test_mixed_list_with_intfloat_list():
-    param_dict = {
-        "_meanMomentum": [100,"200",300],
-        "angle": [1.0,2,3]
-    }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    param_dict = {"_meanMomentum": [100, "200", 300], "angle": [1.0, 2, 3]}
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=1.0'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=1.0'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=200','angle=3'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=1.0'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=2'],
-        [g4bl_cmd,file_name,'_meanMomentum=300','angle=3'],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=1.0"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=1.0"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=200", "angle=3"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=1.0"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=2"],
+        [g4bl_cmd, file_name, "_meanMomentum=300", "angle=3"],
     ]
     assert generated_args == test_args
 
 
 def test_list_with_tuples_of_lists():
-    param_dict = {
-        "_meanMomentum": [100],
-        ("a","b","c"): [[4,5,6],[5,6,7]]
-    }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    param_dict = {"_meanMomentum": [100], ("a", "b", "c"): [[4, 5, 6], [5, 6, 7]]}
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd,file_name,'_meanMomentum=100','a=4','b=5','c=6'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','a=5','b=6','c=7']
+        [g4bl_cmd, file_name, "_meanMomentum=100", "a=4", "b=5", "c=6"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "a=5", "b=6", "c=7"],
     ]
     assert generated_args == test_args
 
+
 def test_tuple_zipl():
-    list1 = [4,5]
-    list2 = [6,7]
+    list1 = [4, 5]
+    list2 = [6, 7]
     param_dict = {
         "_meanMomentum": [100],
-        ("a","b") : [list(v) for v in zip(list1,list2)]
+        ("a", "b"): [list(v) for v in zip(list1, list2)],
     }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd,file_name,'_meanMomentum=100','a=4','b=6'],
-        [g4bl_cmd,file_name,'_meanMomentum=100','a=5','b=7']
+        [g4bl_cmd, file_name, "_meanMomentum=100", "a=4", "b=6"],
+        [g4bl_cmd, file_name, "_meanMomentum=100", "a=5", "b=7"],
     ]
     assert generated_args == test_args
+
 
 def test_singular_tuple():
     param_dict = {
         "sigma": [10],
-        ("Magnet","QUADgradient13", "QUADgradient2", "SOLcurrent") : [["Quadx1.5SolOFF", 0     ,0    ,0]]
+        ("Magnet", "QUADgradient13", "QUADgradient2", "SOLcurrent"): [
+            ["Quadx1.5SolOFF", 0, 0, 0]
+        ],
     }
-    generated_args = generate_args(cmd=g4bl_cmd,param_dict=param_dict,file_name=file_name)
+    generated_args = generate_args(
+        cmd=g4bl_cmd, param_dict=param_dict, file_name=file_name
+    )
     test_args = [
-        [g4bl_cmd, file_name, 'sigma=10', 'Magnet=Quadx1.5SolOFF',"QUADgradient13=0","QUADgradient2=0","SOLcurrent=0"]
+        [
+            g4bl_cmd,
+            file_name,
+            "sigma=10",
+            "Magnet=Quadx1.5SolOFF",
+            "QUADgradient13=0",
+            "QUADgradient2=0",
+            "SOLcurrent=0",
+        ]
     ]
     assert generated_args == test_args
